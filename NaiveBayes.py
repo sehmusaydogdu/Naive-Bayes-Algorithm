06ce3b80c37e849d4e7bdf1fc68d103dc2a58b9b
sehmusaydogdu <sehmusaydogdu47@gmail.com>
2019-10-10 00:19:16 +0300

    added features

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,6 +1,7 @@
 import csv
 trainData =[]
 binaryData=[]
+labelData =[]
 
 def read_csv_data(filename):
     try:
@@ -15,6 +16,16 @@ def read_csv_data(filename):
     except csv.Error:
         print(f'Error reading {filename}')
 
+def read_label_data(filename):
+    try:
+        buffer = None
+        with open(filename) as csvfile:
+            buffer = csv.reader(csvfile, delimiter=',')
+            for row in buffer:
+                labelData.append(int(row[0]))
+    except csv.Error:
+        print(f'Error reading {filename}')
+
 def convert_data_binary():
     for list in trainData:
         temp=[]
@@ -25,6 +36,25 @@ def convert_data_binary():
                 temp.append(1)
         binaryData.append(temp)
 
+def label_probability():
+    female = 0
+    male = 0
+    for label in labelData:
+        if label == 1:
+            female = female+1
+        else:
+            male = male + 1
+
+    total = len(labelData)
+    female = female / total
+    male = male /total
+    print(f'Total label= {total} Female= {female} Male= {male}')
+
+
 if __name__ == '__main__':
     read_csv_data("hw01_images.csv")
+    read_label_data("hw01_labels.csv")
     convert_data_binary()
+    label_probability()
+
+
