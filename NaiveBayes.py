63750864f0010cd1e9d98d5727aa1879524a8318
sehmusaydogdu <sehmusaydogdu47@gmail.com>
2019-10-13 21:57:36 +0300

    add features

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,6 +1,9 @@
 import csv
 import math
 
+import decimal
+decimal.getcontext().prec = 50
+
 train_data   = []  ## set of 200 train data
 test_data    = []  ## set of 200 test  data
 train_label  = []  ## set of 200 train label data
@@ -12,10 +15,8 @@ test_confusion_matrix  = [[0,0],[0,0]]
 female_probability = []
 male_probability = []
 
-epsilon = 0.5
-
-female = 0  ## 0.1
-male = 0    ## 0.9
+female = 0.0
+male   = 0.0
 
 def read_csv_data(filename):
     try:
@@ -51,12 +52,14 @@ def read_label_data(filename):
         print(f'Error reading {filename}')
 
 def train_label_probability():
-    global  female
-    global  male
+    global female
+    global male
     lenght = len(train_label)
     for item in train_label:
-       if item == 1: female = female +1
-       else: male = male + 1
+       if item == 1:
+           female = female + 1
+       else:
+           male = male + 1
     female = female / lenght
     male = male / lenght
 
@@ -73,31 +76,34 @@ def probability_table(numbers):
     male_temp=[]
 
     for index in range(len(numbers)):
-        if train_label[index] == 1: female_list.append(numbers[index])
-        else: male_list.append(numbers[index])
+        if train_label[index] == 1:
+            female_list.append(numbers[index])
+        else:
+            male_list.append(numbers[index])
 
     female_mean = list_mean(female_list)
     female_deviation = list_deviation(female_list,female_mean)
     female_temp.append(female_mean)
     female_temp.append(female_deviation)
-    print(f'Female Mean = {female_mean}  Deviation ={female_deviation} ')
+    print(f'Female-> Mean = {female_mean}  Deviation ={female_deviation} ')
     female_probability.append(female_temp)
 
     male_mean = list_mean(male_list)
     male_deviation = list_deviation(male_list,male_mean)
     male_temp.append(male_mean)
     male_temp.append(male_deviation)
-    print(f'Male Mean = {male_mean}  Deviation ={male_deviation} ')
+    print(f'Male-> Mean = {male_mean}  Deviation ={male_deviation} ')
     male_probability.append(male_temp)
 
-def multiplication(numbers):
-    temp = 1.0
-    for item in numbers:
-        temp = (temp * item * epsilon)
-    return temp
+def probability_log(numbers):
+  total = 0.0
+  for item in numbers:
+      total = total +math.log(item,math.e)
+  return total
 
 def confusion_matrix(data , label, confusion_matrix):
     lenght = len(data)
+    letter = []
     for rowID in range(lenght):
         female_calculator = []
         male_calculator = []
@@ -106,24 +112,28 @@ def confusion_matrix(data , label, confusion_matrix):
             female_calculator.append(gauss_formula(data[rowID][index], female_list[0], female_list[1]))
 
             male_list = male_probability.__getitem__(index)
-            male_calculator.append(gauss_formula(data[rowID][index],male_list[0],male_list[1]))
+            male_calculator.append(gauss_formula(data[rowID][index],male_list[0], male_list[1]))
 
-        f_multiplaciton = multiplication(female_calculator) * female
-        m_multiplaciton = multiplication(male_calculator) * male
+        f_multiplaciton = probability_log(female_calculator) + math.log(female)
+        m_multiplaciton = probability_log(male_calculator) + math.log(male)
 
-        if(f_multiplaciton > m_multiplaciton):  ## Female
+        if f_multiplaciton > m_multiplaciton:
+            letter.append(1)
             if label[rowID] == 1:
                 confusion_matrix[0][0] = confusion_matrix[0][0] + 1
             else:
                 confusion_matrix[0][1] = confusion_matrix[0][1] + 1
-        else: ## Male
+        else:
+            letter.append(2)
             if label[rowID] == 1:
                 confusion_matrix[1][0] = confusion_matrix[1][0] + 1
             else:
                 confusion_matrix[1][1] = confusion_matrix[1][1] + 1
 
+    return letter;
+
 def gauss_formula(data,mean,deviation):
-    result = (1/(deviation*math.sqrt(2*math.pi)))*math.exp((-1)*math.pow((data-mean),2)/(2*math.pow(deviation,2)))
+    result = (1.0/(deviation*math.sqrt(2.0*math.pi)))*math.exp((-1.0)*math.pow((data-mean),2.0)/(2.0*math.pow(deviation,2.0)))
     return result
 
 def list_mean(numbers):
@@ -134,12 +144,12 @@ def list_mean(numbers):
     total = total / lenght
     return total
 
-def list_deviation(numbers,mean):
+def list_deviation(numbers, mean):
     total = 0
     lenght = len(numbers)
     for item in numbers:
         total += (math.pow((item-mean),2))
-    total = total /lenght
+    total = total / lenght
     return math.sqrt(total)
 
 if __name__ == '__main__':
@@ -150,10 +160,14 @@ if __name__ == '__main__':
     transponse_matrix()
     print(f'Probability Female= {female} Male= {male}')
 
-    confusion_matrix(train_data, train_label, train_confusion_matrix)
+    l1 = confusion_matrix(train_data, train_label, train_confusion_matrix)
+    print(l1)
+    print(train_label)
     print(train_confusion_matrix)
     print(f'Train Accuracy : {(train_confusion_matrix[0][0] + train_confusion_matrix[1][1]) / len(train_data)}')
 
-    confusion_matrix(test_data, test_label, test_confusion_matrix)
+    l2 = confusion_matrix(test_data, test_label, test_confusion_matrix)
+    print(l2)
+    print(test_label)
     print(test_confusion_matrix)
     print(f'Test Accuracy : {(test_confusion_matrix[0][0] + test_confusion_matrix[1][1]) / len(test_data)}')
