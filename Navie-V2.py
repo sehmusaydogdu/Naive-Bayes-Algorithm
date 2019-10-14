e96db1038b20736e50dd70a8c6e81ee6756f23ae
Şeyhmus Aydoğdu <sehmusaydogdu47@gmail.com>
2019-10-14 11:49:09 +0300

    Update Navie-V2.py

--- Navie-V2.py
+++ Navie-V2.py
@@ -14,6 +14,7 @@ label_test=[]       ## "hw01_label.csv" dosyasıdaki diğer 200 veriyi test olar
 female=[]
 male=[]
 
+## Deviation Calculator
 def deviation(mean,values):
     result = 0
     for index in range(len(values)):
@@ -21,15 +22,19 @@ def deviation(mean,values):
     result = math.sqrt(result / len(values))
     return result
 
-def gauss(x,mean,deviation):
-    return (1.0/(deviation*math.sqrt(2.0*math.pi)))*math.exp((-1.0)*math.pow((x-mean),2.0)/(2.0*math.pow(deviation,2.0)))
+## Gauss calculator
+def gauss(x,mu,sigma):
+    return (1./(sigma*math.sqrt(2.0*math.pi)))*math.exp((-1.)*math.pow((x-mu),2.)/(2.*math.pow(sigma,2.)))
 
-def log_probability(values):
+## Probability Calculator
+def log_probability_calculator(values,probability):
   sum = 0.0
   for item in values:
       sum = sum + math.log(item,math.e)
+  sum = sum + math.log(probability)
   return sum
 
+## Read file hw01_images_data
 def images_data_read(filename):
     reader= None
     with open(filename, 'r') as csvFile:
@@ -38,6 +43,7 @@ def images_data_read(filename):
             images_data.append(row)
     csvFile.close()
 
+## Divided the data set into two  (test-size = 200, training-size = 200)
 def images_data_split():
     ## trainig data
     for i in range(0,200):
@@ -53,6 +59,7 @@ def images_data_split():
             dummy.append(float(item))
         test.append(dummy)
 
+## Read file hw01_label_data
 def label_data_read(filename):
     reader= None
     with open(filename, 'r') as csvFile:
@@ -61,6 +68,7 @@ def label_data_read(filename):
             label_data.append(row)
     csvFile.close()
 
+## Divided the data set into two  (test-size = 200, training-size = 200)
 def label_data_split():
     ## trainig data
     for i in range(0, 200):
@@ -72,6 +80,7 @@ def label_data_split():
         for item in label_data[i]:
             label_test.append(int(item))
 
+## Training Female probability calculator
 def female_probability():
     count = 0.0
     for data in label_training:
@@ -79,6 +88,7 @@ def female_probability():
             count = count + 1
     return count / len(label_training)
 
+## Training Male probability calculator
 def male_probability():
     count = 0.0
     for data in label_training:
@@ -86,40 +96,44 @@ def male_probability():
             count = count + 1
     return count / len(label_training)
 
-def training_calc():
-    matrix = list(zip(*training))
-    for numbers in matrix:
-        female_and_male_pro_calc(numbers)
+## Female ve Male için her bir (x1,x2,x3,...,x4096) için kolon bazında ortalama ve standart sapma değerlerini hesaplıyorum. Örneğin;
+#       mean  deviation
+# x1     0.1    0.1
+# x2     0.2    0.9
+# x3     0.3    0.1
+# ...
+# x4096  0.5    0.7
+def training_female_and_male_pro_calc():
+    for values in list(zip(*training)):
+        female_dummy=[]
+        female_list =[]
 
-def female_and_male_pro_calc(numbers):
-    female_dummy=[]
-    female_list =[]
+        male_dummy=[]
+        male_list=[]
 
-    male_dummy=[]
-    male_list=[]
+        for i in range(len(values)):
+            if label_training[i] == 1:
+                female_list.append(values[i])
+            else:
+                male_list.append(values[i])
 
-    for i in range(len(numbers)):
-        if label_training[i] == 1:
-            female_list.append(numbers[i])
-        else:
-            male_list.append(numbers[i])
+        mean1 = statistics.mean(female_list)
+        dev1 = deviation(mean1,female_list)
+        print(f'FEMALE {mean1}  {dev1} ')
 
-    mean1 = statistics.mean(female_list)
-    dev1 = deviation(mean1,female_list)
-    print(f'FEMALE {mean1}  {dev1} ')
+        female_dummy.append(mean1)
+        female_dummy.append(dev1)
+        female.append(female_dummy)
 
-    female_dummy.append(mean1)
-    female_dummy.append(dev1)
-    female.append(female_dummy)
+        mean2=statistics.mean(male_list)
+        dev2=deviation(mean2,male_list)
+        print(f'MALE {mean2}  {dev2} ')
 
-    mean2=statistics.mean(male_list)
-    dev2=deviation(mean2,male_list)
-    print(f'MALE {mean2}  {dev2} ')
+        male_dummy.append(mean2)
+        male_dummy.append(dev2)
+        male.append(male_dummy)
 
-    male_dummy.append(mean2)
-    male_dummy.append(dev2)
-    male.append(male_dummy)
-
+## Yeni gelen her bir data için gauss formülünde hesaplıyorum. Hangi olasılık daha büyükse sen ona aitsin diyorum.
 def training_confusion(data,f,m):
     list=[]
     for i in range(len(data)):
@@ -127,11 +141,11 @@ def training_confusion(data,f,m):
         male_calc = []
 
         for j in range(len(data[i])):
-            female_calc.append(gauss(data[i][j], female.__getitem__(i)[0], female.__getitem__(i)[1]))
-            male_calc.append  (gauss(data[i][j],male.__getitem__(i)[0], male.__getitem__(i)[1]))
+            female_calc.append(gauss(data[i][j],female[j][0], female[j][1]))
+            male_calc.append  (gauss(data[i][j],male[j][0], male[j][1]))
 
-        f_sum_log = log_probability(female_calc) + math.log(f)
-        m_sum_log = log_probability(male_calc) + math.log(m)
+        f_sum_log = log_probability_calculator(female_calc,f)
+        m_sum_log = log_probability_calculator(male_calc,m)
 
         if f_sum_log > m_sum_log:
             list.append(1)
@@ -139,6 +153,7 @@ def training_confusion(data,f,m):
             list.append(2)
     return list
 
+## Confisuon matrix calculator
 def calculator_confisuon_matrix(expected,actual):
     matrix = [[0, 0], [0, 0]]
     for index in range(len(expected)):
@@ -162,7 +177,7 @@ label_data_split()
 female_prob = female_probability()
 male_prob = male_probability()
 
-training_calc()
+training_female_and_male_pro_calc()
 
 expected_train_list= training_confusion(training,female_prob,male_prob)
 expected_test_list = training_confusion(test,female_prob,male_prob)
